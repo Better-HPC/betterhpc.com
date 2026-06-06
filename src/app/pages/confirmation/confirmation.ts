267e2400df3464d6be6b3adb59f494a562fa4f3c
Daniel Perrefort <djperrefort@gmail.com>
2026-06-06 08:09:58 -0400

    Adds content to confirmation page

--- src/app/pages/confirmation/confirmation.ts
+++ src/app/pages/confirmation/confirmation.ts
@@ -1,9 +1,14 @@
 import { Component } from "@angular/core";
+import { RouterLink } from "@angular/router";
+import { SectionComponent } from "../../layout/section/section";
 
 /** Email sent confirmation page. */
 @Component({
   selector: "app-confirmation-page",
-  imports: [],
+  imports: [
+    SectionComponent,
+    RouterLink
+  ],
   templateUrl: "confirmation.html",
 })
 export class ConfirmationPage {}
